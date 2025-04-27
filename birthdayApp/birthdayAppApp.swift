99059609cdd7f1898090131cbe93973bb6a265b2
aditi <Adishanu4u@gmail.com>
2025-04-27 16:41:55 -0700

    Initial Commit

---
+++ birthdayApp/birthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthdayAppApp.swift
+//  birthdayApp
+//
+//  Created by Aditi Agarwal on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct birthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
