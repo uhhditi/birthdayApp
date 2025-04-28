7ed92be9beeb14c3cfe2a4e6cf3a1518951eb3d8
aditi <Adishanu4u@gmail.com>
2025-04-27 17:05:22 -0700

    Created birthday app that saves names and birthdays of friends and allows updates+ displays information.

--- birthdayApp/ContentView.swift
+++ birthdayApp/ContentView.swift
@@ -1,24 +1,46 @@
-//
-//  ContentView.swift
-//  birthdayApp
-//
-//  Created by Aditi Agarwal on 4/27/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Ruhi", birthday: Date(timeIntervalSince1970: 36)),
+        Friend(name: "Sophia", birthday: Date())
+    ]
+    
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker("Birthday", selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date)
+                    TextField("Name", text: $newName)
+                        .textFieldStyle(.roundedBorder)
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                }
+                .padding()
+                .background(.bar)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
---
+++ birthdayApp/Friend.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    // Initializer for the Friend class
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
+
